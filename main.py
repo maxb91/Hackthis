bdec5d4cb2de6548b1704a8cf768ca1434cd2998
Maximilian Brunner <mabrunne@student.ethz.ch>
2018-09-15 23:06:44 +0200

    Added general overview, now directs directly from entry to list view.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 import pandas as pd
 from data import Foods
@@ -66,21 +66,31 @@ def about():
 def view_list():
   print "Querying"
   purch = Purchases.query.all()
-  ref_val_unit = ReferenceValuesUnits.query.first()
-  units = [ref_val_unit.globalWarmingPotential, ref_val_unit.energyConsumption, ref_val_unit.waterUsage]
+  units = ['kg of CO2', 'MJ', 'm^3']
   return render_template('view_list.html', purchases = purch, ref_units = units)
+
+@app.route('/overview')
+def overview():
+  print "Querying"
+  purch = ReferenceValues.query.all()
+  units = ['kg of CO2', 'MJ', 'm^3']
+  return render_template('overview.html', purchases = purch, ref_units = units)
 
 @app.route('/result', methods = ['GET', 'POST'])
 def result():
   amount = float(request.form['text'])
   sel_category = request.form['foodtype']
   ref_val = ReferenceValues.query.filter_by(category=sel_category).first()
-  purchase = Purchases(category = sel_category, amount = amount, globalWarmingPotential = amount*ref_val.globalWarmingPotential, 
-                        energyConsumption = amount*ref_val.energyConsumption, waterUsage = amount*ref_val.waterUsage)
+  print ref_val.energyConsumption
+  if ref_val.energyConsumption == None:
+    ref_val.energyConsumption = 0
+    ref_val.waterUsage = 0
+  purchase = Purchases(category = sel_category, amount = amount, globalWarmingPotential = round(amount*ref_val.globalWarmingPotential,2), 
+                        energyConsumption = round(amount*ref_val.energyConsumption,2), waterUsage = round(amount*ref_val.waterUsage,2))
   db.session.add(purchase)
-  db.session.commit()  
+  db.session.commit()
   print "Added."
-  return("Added item.")
+  return redirect("view_list", code=302)
 
 if __name__ == '__main__':
 	app.run(debug = True)
