0500406fed9bb6de14489a3a1fe52d9f257ca86a
LucaB <bischoffpool.luca@gmail.com>
2018-09-15 04:45:21 +0200

    Reading from dropdown and textfield.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import pandas as pd
 
 EmissonData = pd.read_excel('EmissionValues.xlsx')
@@ -9,6 +9,11 @@ app = Flask(__name__)
 def index():
 	Selections = EmissonData['Category']
 	return render_template('basic.html', selections = Selections)
+
+@app.route('/test', methods = ['GET', 'POST'])
+def result():
+	print ("printing results")
+	return(request.form['colours'] + ' | ' + request.form['text'])
 
 if __name__ == '__main__':
 	app.run(debug = True)
