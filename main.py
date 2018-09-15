6883c7c32a6046f6916c99c59fc8d687b1f465f3
LucaB <bischoffpool.luca@gmail.com>
2018-09-15 03:59:32 +0200

    Added reading from excel via panda.

--- main.py
+++ main.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template
+import pandas as pd
+
+EmissonData = pd.read_excel('EmissionValues.xlsx')
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-	return render_template('basic.html')
+	Selections = EmissonData['Category']
+	return render_template('basic.html', selections = Selections)
 
 if __name__ == '__main__':
 	app.run(debug = True)
