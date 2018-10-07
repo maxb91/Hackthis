8a993f63878fe0e41a5de60c63703939507ec330
LucaB <bischoffpool.luca@gmail.com>
2018-10-07 13:57:33 +0200

    Dynamic table rendering and results are displayed as groups

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, abort
 from flask_sqlalchemy import SQLAlchemy
 import pandas as pd
 from data import Foods
-import datetime
+import datetime, json
 
 
 isDebug = True
@@ -72,14 +72,23 @@ def view_list():
 
 @app.route('/results', methods = ['POST'])
 def result():
-  amount = float(request.form['text'])
-  sel_category = request.form['foodtype']
-  ref_val = ReferenceValues.query.filter_by(category=sel_category).first()
-  purchase = Purchases(category = sel_category, amount = amount, globalWarmingPotential = amount*ref_val.globalWarmingPotential, 
-                        energyConsumption = amount*ref_val.energyConsumption, waterUsage = amount*ref_val.waterUsage)
-  db.session.add(purchase)
-  db.session.commit()  
-  return("Added item.")
+  # Looping through the received object and adding all the elements to the array newData
+  newData = []
+  # Getting the response and converting it to an object
+  postData = json.loads(request.form['purchases'])
+  # Looping through the dict and adding the values to the newData list
+  for key in postData:
+    ref_val = ReferenceValues.query.filter_by(category=key).first()
+    # Making sure to not multiply by 'NaN'
+    if ref_val.energyConsumption == None:
+      ref_val.energyConsumption = 0
+      ref_val.waterUsage = 0
+    newData.append(Purchases(category = key, amount = postData[key], globalWarmingPotential = round(postData[key]*ref_val.globalWarmingPotential,2), 
+                        energyConsumption = round(postData[key]*ref_val.energyConsumption,2), waterUsage = round(postData[key]*ref_val.waterUsage,2)))
+  
+  db.session.bulk_save_objects(newData)
+  db.session.commit()
+  return json.dumps({'success':True}), 200, {'ContentType':'application/json'}
 
 @app.route('/piechart')
 def piechart():
@@ -98,10 +107,50 @@ def overview():
   units = [ref_val_unit.globalWarmingPotential, ref_val_unit.energyConsumption, ref_val_unit.waterUsage]
   return render_template('overview.html', purchases = purch, ref_units = units)  
 
-@app.route('/testData')
+@app.route('/testData', methods=['GET'])
 def provideList():
-  return "Yallo"
-
+  purch = Purchases.query.all()
+  groupedResponse = {}
+  databaseCategories = ['category', 'amount', 'globalWarmingPotential', 'energyConsumption', 'waterUsage']
+  # Looping through the dict and grouping to dicts based on the parameter sent
+  if request.args.get('option') == 'weekly':
+    for lines in purch:
+      current = datetime.datetime.isocalendar(lines.dateAdded)[1]
+      singleEntry = {}
+      for keys in databaseCategories:
+        singleEntry[keys] = getattr(lines, keys)
+      singleEntry['dateAdded'] = lines.dateAdded.strftime("%Y-%m-%d")
+      if current in groupedResponse:
+        groupedResponse[current].append(singleEntry)
+      else:
+        groupedResponse[current] = [singleEntry]
+    return json.dumps([{keys: groupedResponse[keys]} for keys in groupedResponse])
+  elif request.args.get('option') == 'daily':
+    for lines in purch:
+      current = datetime.datetime.isocalendar(lines.dateAdded)[-1]
+      singleEntry = {}
+      for keys in databaseCategories:
+        singleEntry[keys] = getattr(lines, keys)
+      singleEntry['dateAdded'] = lines.dateAdded.strftime("%Y-%m-%d")
+      if current in groupedResponse:
+        groupedResponse[current].append(singleEntry)
+      else:
+        groupedResponse[current] = [singleEntry]
+    return json.dumps([{keys: groupedResponse[keys]} for keys in groupedResponse])
+  elif request.args.get('option') == 'yearly':
+    for lines in purch:
+      current = datetime.datetime.isocalendar(lines.dateAdded)[0]
+      singleEntry = {}
+      for keys in databaseCategories:
+        singleEntry[keys] = getattr(lines, keys)
+      singleEntry['dateAdded'] = lines.dateAdded.strftime("%Y-%m-%d")
+      if current in groupedResponse:
+        groupedResponse[current].append(singleEntry)
+      else:
+        groupedResponse[current] = [singleEntry]
+    return json.dumps([{keys: groupedResponse[keys]} for keys in groupedResponse])
+  else:
+    return abort(400)
 if __name__ == '__main__':
   if isDebug == True:
     app.run(debug = True, host='0.0.0.0')
